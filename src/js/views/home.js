6b8e5dd81746174b354ad2f02fda5d352f395375
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-07-25 17:13:10 +0000

    Ensuring comfortable usage of workspace

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -8,45 +8,34 @@ export const Home = () => {
   const [starships, setStarships] = useState([]);
 
   useEffect(async () => {
-	const result = await Promise.all(
-		["people","planets","starships"].map((url) =>(
-			fetch(`https://swapi.dev/api/${url}`)
-		))
-	)
-	const data = await Promise.all(result.map((resp) => resp.json()))
-	
+    const result = await Promise.all(
+      ["people", "planets", "starships"].map((url) =>
+        fetch(`https://swapi.dev/api/${url}`)
+      )
+    );
+    const data = await Promise.all(result.map((resp) => resp.json()));
   }, []);
 
+  return characters.map((character, idx) => (
+    <Card
+      key={idx}
+      img={`https://starwars-visualguide.com/assets/img/characters/${
+        character.url.split("/")[5]
+      }.jpg`}
+    >
+      <h5 className="card-title">{character.name}</h5>
+      <ul className="list-group">
+        <li className="list-group-item">Gender: {character.gender}</li>
+        <li className="list-group-item">Hair Color: {character.hair_color}</li>
 
-  return (
-	
-	
-	
-	
-	
-	characters.map((character, idx) => (  
-	  <Card
-        key={idx}
-        img={`https://starwars-visualguide.com/assets/img/characters/${
-          character.url.split("/")[5]
-        }.jpg`}
-      >
-        <h5 className="card-title">{character.name}</h5>
-        <ul className="list-group">
-          <li className="list-group-item">Gender: {character.gender}</li>
-          <li className="list-group-item">
-            Hair Color: {character.hair_color}
-          </li>
-
-          <li className="list-group-item">Eye Color: {character.eye_color}</li>
-        </ul>
-        <span className="d-flex justify-content-space-between">
-          <a className="btn btn-outline-primary">Learn More!</a>
-          <a className="btn btn-outline-warning">
-            <i class="fa-regular fa-heart"></i>
-          </a>
-        </span>
-      </Card>
-
-  )));
+        <li className="list-group-item">Eye Color: {character.eye_color}</li>
+      </ul>
+      <span className="d-flex justify-content-space-between">
+        <a className="btn btn-outline-primary">Learn More!</a>
+        <a className="btn btn-outline-warning">
+          <i class="fa-regular fa-heart"></i>
+        </a>
+      </span>
+    </Card>
+  ));
 };
